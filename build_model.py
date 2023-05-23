d32b067deac4a854e9b0c01ecdf5b8200dd05acd
Deeptanshu Sekhri <deeptanshusekhri9@gmail.com>
2023-05-23 17:36:50 +0100

    removing old model and changes to build model

--- build_model.py
+++ build_model.py
@@ -2,29 +2,34 @@ import tensorflow_hub as hub
 import os
 import tensorflow as tf
 from bert import tokenization
-import pandas as pd
 import argparse
-from model import bert_encode, build_model
+from model import bert_encode
 
-def processor(model):
-  
-  @tf.funtion(input_signature=[tf.TensorSpec([None], tf.string)])
+module_url = "https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/1"
+bert_layer = hub.KerasLayer(module_url, trainable=True)
+bert_layer_frozen = hub.KerasLayer(module_url, trainable=False)
+vocab_file = bert_layer.resolved_object.vocab_file.asset_path.numpy()
+do_lower_case = bert_layer.resolved_object.do_lower_case.numpy()
+tokenizer = tokenization.FullTokenizer(vocab_file, do_lower_case)
+
+def processor_model(model):
+  @tf.function(input_signature=[tf.TensorSpec(shape=(), dtype=tf.string)])
   def emotional(text):
-     module_url = "https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/1"
-     bert_layer = hub.KerasLayer(module_url, trainable=True)
-     bert_layer_frozen = hub.KerasLayer(module_url, trainable=False)
-
-     vocab_file = bert_layer.resolved_object.vocab_file.asset_path.numpy()
-     do_lower_case = bert_layer.resolved_object.do_lower_case.numpy()
-     tokenizer = tokenization.FullTokenizer(vocab_file, do_lower_case)
-     tokenizer = tokenization.FullTokenizer(vocab_file, do_lower_case)
-     predict_input = bert_encode(text, tokenizer, max_len=100)
-     output = model(predict_input)
+     global tokenizer
+     print(type(text))
+     #text = tf.strings.(text)
+     #text = text.decode("utf-8")
+     #TODO: fix encoder input text
+     predict_input = bert_encode("my test", tokenizer, max_len=100)
+     prediction = model(predict_input)
+     #prediction = model(text)
      labels=['affection','anticipation','anxious','confusion','curiosity','delight','desire',
              'frustration','melancholy','mortification','relief','respect','revelation','neutral']
-     predict_output = labels[output.argmax(axis=-1)]
+     indices = tf.argmax(prediction, axis=-1)  # Index with highest prediction
+     label = tf.gather(params=labels, indices=indices)  # Class name
+     return label
   
-     return predict_output
+  return emotional
 
 def main(args):
   
@@ -32,33 +37,37 @@ def main(args):
   model = tf.keras.models.load_model(args.load_model_paths)
   tf.saved_model.save(
                       model,export_dir=os.path.join(args.save_model_dir,str(args.version)),
-                      signatures={"serving_default": processor(model)},
+                      signatures={"serving_default": processor_model(model)},
                       )
 
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
+  parser = argparse.ArgumentParser(
+      formatter_class=argparse.ArgumentDefaultsHelpFormatter
+  )
   # Input Arguments
   parser.add_argument(
-      '--load_model_paths',
+      "--load_model_paths",
       help='path to model to load',
-      required=True
+      type=str,
+      default="/Users/nehasachdeva/Documents/MSc-AI_Docs/NLP/coursework/deeptanshu_NLP_CW/emo-class/models/golden/",
   )
   parser.add_argument(
       '--save_model_dir',
       help="""\
       Directory to save model in.\
       """,
-      required=True
-)
+      type=str,
+      default="/Users/nehasachdeva/Documents/MSc-AI_Docs/NLP/coursework/deeptanshu_NLP_CW/emo-class/models/deploy/"
+  )
   parser.add_argument(
       '--version',
       help="""\
       Version of the model.\
       """,
       type=int,
-      required=True
-)
+      default="1",
+  )
   args = parser.parse_args()
   arguments = args.__dict__
   main(args)
